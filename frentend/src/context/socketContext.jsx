de0f1e12b8778d0a34c89976cfa26b316815668e
Rehman-Babar327 <153273576+Rehman-Babar@users.noreply.github.com>
2024-03-26 19:34:00 +0500

    Update socketContext.jsx

--- frentend/src/context/socketContext.jsx
+++ frentend/src/context/socketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-x4fw.onrender.com/", {
+			const socket = io("https://chat-app-x4fw.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
