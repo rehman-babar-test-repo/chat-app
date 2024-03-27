8b6a62c649e145f4313f2c53699b13491489bf5a
Rehman-Babar327 <153273576+Rehman-Babar@users.noreply.github.com>
2024-03-27 09:27:03 +0500

    Update jwtTokens.js

--- backend/utils/jwtTokens.js
+++ backend/utils/jwtTokens.js
@@ -8,6 +8,7 @@ const genratreTokenAndSetCookie =(userId, res)=>{
         maxAge: 15*24*60*60*1000,
         httpOnly: true,
         sameSite: "strict",
+        secure:process.env.NODE_ENV !== 'develpment'
         
     })
 }
